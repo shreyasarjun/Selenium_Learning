1435f0c265e4912c483130cbe8269e1859f33509
shreyasarjun <shreyas.arjun007@gmail.com>
2015-08-22 21:51:11 +0530

    First Commit

---
+++ 03JULY2015/src/AreaofRadius.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class AreaofRadius {
+	static double radius;
+
+	double calculate(int b){
+		
+		radius=Math.PI*b*b;
+		return radius;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner temp=new Scanner(System.in);
+		
+		System.out.println("Please enter the radius value");
+		
+		int a=temp.nextInt();
+		
+		AreaofRadius obj=new AreaofRadius();
+		obj.calculate(a);
+		
+		System.out.println("Area of radius is "+radius);
+		
+
+	}
+
+}
