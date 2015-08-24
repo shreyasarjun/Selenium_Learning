42230e286bb58ac473f0476265dd1b9426e18bef
shreyasarjun <shreyas.arjun007@gmail.com>
2015-08-24 07:42:34 +0530

    Main Method

---
+++ 03JULY2015/src/HotelBooking.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class HotelBooking extends TestDriverScript{
+	
+	@BeforeMethod
+	
+	public void browseropening () throws IOException, InterruptedException
+	{
+		fetchingurl();
+	}
+	
+	@Test
+	
+	public void calculation()
+	{
+		System.out.println("operation success");
+	}
+
+}
