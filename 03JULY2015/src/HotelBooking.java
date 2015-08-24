f1e091821c10e50ef38a170fb4add6df5dae2a26
shreyasarjun <shreyas.arjun007@gmail.com>
2015-08-24 08:05:53 +0530

    ..

--- 03JULY2015/src/HotelBooking.java
+++ 03JULY2015/src/HotelBooking.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class HotelBooking extends TestDriverScript{
 	
-	@BeforeMethod
+	@BeforeTest
 	
 	public void browseropening () throws IOException, InterruptedException
 	{
