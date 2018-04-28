62aadf84b09d0c0fcacd6f0bae2330274d92d8ef
shreyasarjun <shreyas.arjun007@gmail.com>
2018-04-28 19:40:38 +0530

    Update Selenium.java
    M.G Road Changes Added

--- 03JULY2015/src/Selenium.java
+++ 03JULY2015/src/Selenium.java
@@ -3,8 +3,9 @@ public class Selenium {
 	
 	public static void main(String []args)
 	{
-		System.out.println("I am in Bangalore now");
-		System.out.println("I am in Mysore now");
+		System.out.println("I am in Bangalore now2");
+		System.out.println("I am in Mysore now2");
+		System.out.println("I am in M.G Road now");
 	}
 
 }
